b06b2dcd307784ecfeb9d219eac143d473976e0c
muchomor32 <barteksokal01@gmail.com>
2024-07-12 12:28:37 +0200

    Update main.py
    readability

--- Rock Paper Scissors Game/main.py
+++ Rock Paper Scissors Game/main.py
@@ -15,15 +15,19 @@ ID = {
 }
 
 while keepPlaying in ["yes", "y"]:
+    
     # user input and random gen
     pInput = input("Chose rock, paper or scissors: ")
     pInput = pInput.lower()
     bInput = randrange(1, 4)
+    
     # correct input check
     if pInput in ["rock", "r", "paper", "p", "scissors", "s"]:
+        
         # display enemy choice
         bChoice = list(ID.keys())
         print("The enemy chose " + str(bChoice[bInput - 1]) + "\n")
+        
         # check if you won or lost
         if ID[pInput] - bInput in [-1, 2]:
             bScore += 1
@@ -36,6 +40,7 @@ while keepPlaying in ["yes", "y"]:
     else:
         print("That is not a valid input\n")
         continue
+        
     # check for if the player wants to keep playing
     keepPlaying = input("\nDo you want to play again? (yes/no)\n")
 
